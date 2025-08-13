b9aa3e6d368d014cbc49a8bbc040b0be39fe7777
“Alihossein <alih.sepahvand@gmail.com>
2025-08-13 14:44:28 -0600

    refactor: update PostCSS configuration and clean up global styles; adjust Canvas component structure

--- my-app-1/src/components/canvas.tsx
+++ my-app-1/src/components/canvas.tsx
@@ -3,14 +3,14 @@
 import { Canvas } from "@react-three/fiber"
 
 export default function CanvasView() {
-    return (
-    <>
+return (
+    <div className="w-full h-full bg-gray-900">
         <Canvas>
             <mesh>
                 <torusKnotGeometry />
                 <meshNormalMaterial />
             </mesh>
         </Canvas>
-    </>
+    </div>
 )
 }
