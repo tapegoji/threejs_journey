b7616ba58d5ae854eecef2549debf7605dd85a4d
“Alihossein <alih.sepahvand@gmail.com>
2025-08-13 16:41:54 -0600

    feat: enhance MainCanvas with custom orbit controls and geometry management

--- my-app-1/src/components/MainCanvas.tsx
+++ my-app-1/src/components/MainCanvas.tsx
@@ -1,47 +1,100 @@
 'use client'
-
-import { Canvas } from "@react-three/fiber"
+import { useRef } from "react"
+import * as THREE from "three"
+import { Canvas,
+        extend,
+        useThree,
+        useFrame} from "@react-three/fiber"
 import { GizmoHelper,
         GizmoViewport,
-        OrbitControls } from "@react-three/drei"
+        // OrbitControls, 
+        PivotControls} from "@react-three/drei"
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
-type ModelProps = {
-    name: string;
-};
+extend({ OrbitControls })
 
-const Model = ({ name }: ModelProps) => {
-    if (name === "SphereGeometry") {
-        return <SphereGeometry />;
-    }
-    if (name === "BoxGeometry") {
-        return <BoxGeometry />;
-    }
-    return null;
+declare module '@react-three/fiber' {
+  interface ThreeElements {
+    orbitControls: any
+  }
 }
 
-const SphereGeometry = () => (
-<mesh scale={1.5} position={[0, 0, 0]} rotateY={Math.PI / 4}>
-    <sphereGeometry args={[1.5, 32, 32]}/>
-    <meshBasicMaterial color="mediumpurple" wireframe/>
-</mesh>
+const Model = ({ name, meshRef }: { name: string, meshRef?: any }) => {
+    const components = {
+        SphereGeometry: <SphereGeometry meshRef={meshRef} />,
+        BoxGeometry: <BoxGeometry meshRef={meshRef} />,
+        PlaneGeometry: <PlaneGeometry meshRef={meshRef} />
+    };
+    return (
+        <>
+        {/* <PivotControls anchor={[1, 1, 1]}> */}
+            {components[name as keyof typeof components] || null}
+        {/* </PivotControls> */}
+        </>
+    );
+}
+
+const SphereGeometry = ({ meshRef }: { meshRef?: any }) => (
+    <mesh ref={meshRef} scale={1.0} position-x={-2}>
+        <sphereGeometry />
+        <meshBasicMaterial color="orange"/>
+    </mesh>
 )
 
-const BoxGeometry = () => (
-    <mesh position={[0, 0, 0]} rotation-z={Math.PI / 4}>
-        <boxGeometry/>
+const BoxGeometry = ({ meshRef }: { meshRef?: any }) => (
+    <mesh ref={meshRef} scale={1.5} position-x={2} rotation-y={Math.PI * 0.25}>
+        <boxGeometry />
         <meshBasicMaterial color="mediumpurple" />
     </mesh>
 )
 
+const PlaneGeometry = ({ meshRef }: { meshRef?: any }) => (
+    <mesh ref={meshRef} scale={10.0} position-y={-1} rotation-x={-Math.PI * 0.5}>
+        <planeGeometry />
+        <meshBasicMaterial color="greenyellow" side={THREE.DoubleSide} />
+    </mesh>
+)
+const useRefs = () => ({
+    boxRef: useRef(null),
+    sphereRef: useRef(null),
+    planeRef: useRef(null),
+    groupRef: useRef(null)
+})
+
+const AnimationLoop = ({ meshRef, rotationAxis }: 
+    { meshRef: any, rotationAxis: 'x' | 'y' | 'z' }) => {
+    useFrame((state, delta) => {
+        if (meshRef.current) {
+            meshRef.current.rotation[rotationAxis] += delta;
+        }
+    });
+    return null;
+}; 
+
+const CustomOrbitControls = () => {
+    const {
+    camera,
+    gl: { domElement },
+    } = useThree()
+    return <orbitControls args={[camera, domElement]} />
+}
+
 export default function MainCanvas() {
+    const { boxRef, sphereRef, planeRef, groupRef } = useRefs()
+
 return (
     <Canvas>
-        <Model name="SphereGeometry" />
-        <Model name="BoxGeometry" />
-        <OrbitControls enableDamping={false} />
+        <group ref={groupRef}>
+            <Model meshRef={sphereRef} name="SphereGeometry" />
+            <Model meshRef={boxRef} name="BoxGeometry" />
+        </group>
+        <Model meshRef={planeRef} name="PlaneGeometry" />
+        {/* <OrbitControls enableDamping={false} /> */}
+        <CustomOrbitControls />
         <GizmoHelper>
             <GizmoViewport />
         </GizmoHelper>
+        <AnimationLoop meshRef={boxRef} rotationAxis="y" />
     </Canvas>
 )
 }
