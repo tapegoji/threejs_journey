9a392931595373c21cfc3c01a28eab83f2579e23
“Alihossein <alih.sepahvand@gmail.com>
2025-08-16 20:41:59 -0600

    feat: add selectable objects with hover and click interactions in the scene

--- 56-r3f-drei/src/index.jsx
+++ 56-r3f-drei/src/index.jsx
@@ -2,9 +2,10 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { createXRStore, XR, XROrigin, useXR, useXRInputSourceState } from '@react-three/xr'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, GizmoHelper, GizmoViewport } from '@react-three/drei'
+import { PivotHandles } from '@react-three/handle'
 import * as THREE from 'three'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 function JoystickLocomotion({ children, speed = 2, ...props }) {
     const ref = useRef()
@@ -67,6 +68,83 @@ function JoystickLocomotion({ children, speed = 2, ...props }) {
     )
 }
 
+function SelectableObject({ children, position, scale, objectId, selectedObject, setSelectedObject, color = "white", ...props }) {
+    const meshRef = useRef()
+    const [hovered, setHovered] = useState(false)
+    const isSelected = selectedObject === objectId
+    
+    const handleClick = () => {
+        setSelectedObject(isSelected ? null : objectId)
+    }
+    
+    return (
+        <PivotHandles
+            size={1}
+            hidden={!isSelected}
+        >
+            <mesh
+                ref={meshRef}
+                position={position}
+                scale={scale}
+                onClick={handleClick}
+                onPointerEnter={() => setHovered(true)}
+                onPointerLeave={() => setHovered(false)}
+                {...props}
+            >
+                {children}
+                <meshStandardMaterial 
+                    color={hovered ? "hotpink" : color}
+                    emissive={isSelected ? "purple" : "black"}
+                    emissiveIntensity={isSelected ? 0.2 : 0}
+                />
+            </mesh>
+        </PivotHandles>
+    )
+}
+
+function Scene() {
+    const [selectedObject, setSelectedObject] = useState(null)
+    const { session } = useXR()
+    const isInXR = !!session
+    
+    return (
+        <>
+            <JoystickLocomotion position={[1, 1, 1]} speed={2} />
+            <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
+            <ambientLight intensity={ 1.5 } />
+
+            <SelectableObject 
+                position={[-2, 0, 0]} 
+                objectId="sphere"
+                selectedObject={selectedObject}
+                setSelectedObject={setSelectedObject}
+                color="orange"
+            >
+                <sphereGeometry />
+            </SelectableObject>
+
+            <SelectableObject 
+                position={[2, 0, 0]} 
+                scale={1.5}
+                objectId="cube"
+                selectedObject={selectedObject}
+                setSelectedObject={setSelectedObject}
+                color="mediumpurple"
+            >
+                <boxGeometry />
+            </SelectableObject>
+
+            <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+                <planeGeometry />
+                <meshStandardMaterial color="greenyellow" />
+            </mesh>
+            
+            {/* OrbitControls only outside XR */}
+            {!isInXR && <OrbitControls enableDamping makeDefault />}
+        </>
+    )
+}
+
 const store = createXRStore()
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
@@ -76,29 +154,9 @@ root.render(
         <button onClick={() => store.enterVR()}>Enter VR</button>
         <button onClick={() => store.enterAR()}>Enter AR</button>
         <Canvas>
-        <XR store={store}>
-            <JoystickLocomotion position={[1, 1, 1]} speed={2} />
-                {/* <LeftStickLocomotion speed={2} /> */}
-            <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-            <ambientLight intensity={ 1.5 } />
-
-            <mesh position-x={ - 2 }>
-                <sphereGeometry />
-                <meshStandardMaterial color="orange" />
-            </mesh>
-
-            <mesh position-x={ 2 } scale={ 1.5 }>
-                <boxGeometry />
-                <meshStandardMaterial color="mediumpurple" />
-            </mesh>
-
-            <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-                <planeGeometry />
-                <meshStandardMaterial color="greenyellow" />
-            </mesh>
-                {/* OrbitControls only outside XR; disable damping to avoid conflict */}
-                {/* <OrbitControls enableDamping={false} makeDefault /> */}
-        </XR>
+            <XR store={store}>
+                <Scene />
+            </XR>
         </Canvas>
     </>
 )
